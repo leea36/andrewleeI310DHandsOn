3dd8196de1d747d16bcf9f48429064500e5f91dc
Andrew Lee <drew@Andrews-MacBook-Pro.local>
2023-09-14 22:18:29 -0500

    radius1 and radius2 changed on area_of_circle & radius1 and radius2 changed on volume_of_sphere

--- volume_of_a_sphere.py
+++ volume_of_a_sphere.py
@@ -4,3 +4,10 @@ def compute_volume_of_sphere(radius):
 	return volume
 
 
+radius1 = 30
+volume1 = compute_volume_of_sphere(radius1)
+print(f"The volume of sphere with radius {radius1} is: {volume1}")
+
+radius2 = 40
+volume2 = compute_volume_of_sphere(radius2)
+print(f"The volume of sphere with radius {radius2} is: {volume2}")
